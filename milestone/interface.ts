4b86d8055546dc63eba93e4b806e3db30db85bb6
denvenum1 <147246499+denvenum1@users.noreply.github.com>
2024-02-29 20:31:00 +0000

    added interfaces and objects

---
+++ milestone/interface.ts
@@ -0,0 +1,24 @@
+export interface Valorant {
+    characters: Character[];
+}
+
+export interface Character {
+    id:             string;
+    name:           string;
+    description:    string;
+    age:            number;
+    onMission:      boolean;
+    birthdate:      Date;
+    country:        string;
+    profilePicture: string;
+    abilities:      string[];
+    role:           string;
+    teams:          Teams;
+}
+
+export interface Teams {
+    id:      string;
+    name:    string;
+    logo:    string;
+    players: string[];
+}
