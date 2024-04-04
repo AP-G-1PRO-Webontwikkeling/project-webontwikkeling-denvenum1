24ccb9fe7ff5cbe331683b527a1d798fed1ac488
denvenum1 <147246499+denvenum1@users.noreply.github.com>
2024-04-04 11:32:45 +0000

    roleSymbol in interface en json

--- milestone.ts
+++ milestone.ts
@@ -1,15 +1,16 @@
 import express from "express";
-
+fetchData()
 async function fetchData() {
     
     try {
-        const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-denvenum1/main/milestone/characters.json");
+        const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-denvenum1/main/characters.json");
         
         if (!response.ok) {
             throw new Error("could not fetch resource");
             
         }
         const data = await response.json();
+        console.log(data.characters[0])
         return data;
     } catch (error) {
         console.log(error);
@@ -17,12 +18,11 @@ async function fetchData() {
 }
 
 const app = express();
-app.set("port", 3001);
+app.set("port", 3000);
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.get("/", async (req, res) => {
     const data = await fetchData();
-    
     res.render("index", { data: data });
 });
 
